00f2e4d7a2f62e8edd5c8e363429a0b14a58c968
ReyamEhab <144321212+ReyamEhab@users.noreply.github.com>
2023-10-02 13:30:29 +0400

    chapter 1 practise 5

---
+++ Chapter 1- Getting Started with Python/Exercises/practise 5.py
@@ -0,0 +1,6 @@
+# Write four strings in different variables and print the outpot as one string. (String Concatention)
+name =" Reyam "
+age ="20 "
+univercity ="bathspa univercity "
+major ="creative computing "
+print (name+age+univercity+major)
