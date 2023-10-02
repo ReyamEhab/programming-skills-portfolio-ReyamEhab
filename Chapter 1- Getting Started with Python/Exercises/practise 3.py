f18324f8e5d90611da04880f21347668f0515d17
ReyamEhab <144321212+ReyamEhab@users.noreply.github.com>
2023-10-02 12:21:24 +0400

    chapter 1 practise 3

---
+++ Chapter 1- Getting Started with Python/Exercises/practise 3.py
@@ -0,0 +1,16 @@
+'''You need to make a program for a leader board. 
+The program needs to outpot the numbers 1 to 9,each on a 
+seperate line followed by a dot'''
+print("""
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+""")
+
+
