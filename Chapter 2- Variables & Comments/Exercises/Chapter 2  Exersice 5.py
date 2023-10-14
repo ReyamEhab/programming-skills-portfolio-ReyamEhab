b04d5c3d93cbab3d90691c20e284bc47925d2920
ReyamEhab <reyam_kama26@yahoo.com>
2023-10-14 23:15:39 +0400

    Chapter 2 Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter 2  Exersice 5.py
@@ -0,0 +1,5 @@
+total_money = 50
+usb_price = 6
+num_usb = total_money // usb_price
+remaining_money = total_money % usb_price
+print("She can buy " + str(num_usb) + " USB sticks and will have " + str(remaining_money) + " pounds left.")
