3adf29e2055b5e52e4fadcc2c00c621b148a8f07
ReyamEhab <reyam_kama26@yahoo.com>
2023-10-16 14:01:27 +0400

    Chapter 3 - Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 4.py
@@ -0,0 +1,10 @@
+guests = ['Kamal', 'Ahmed', 'Mariam']
+
+name_0 = guests[0]
+print(name_0 + " please come to dinner.")
+
+name_1 = guests[1]
+print(name_1 + " please come to dinner.")
+
+name_2 = guests[2]
+print(name_2 + " please come to dinner.")
---
+++ Chapter 3- Data Structures/Exercises/Chapter 3 -Exercise 3.py
@@ -0,0 +1,11 @@
+List = ['Motorcycle', 'Car','bicycle']
+
+name_0 = List[0]
+print( "I would like to own a Suzuki " + name_0 )
+
+name_1 = List[1]
+print( "I would like to own a Tisla " + name_1 )
+
+name_2 = List[2]
+print( "I would like to own a beautiful " + name_2 )
+ 
