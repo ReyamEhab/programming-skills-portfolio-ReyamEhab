819b2fcf1cef9c131db429e25be1aa2490882af2
ReyamEhab <reyam_kama26@yahoo.com>
2023-10-16 09:03:50 +0400

    Chapter 3 -Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Chapter 3 -Exercise 2.py
@@ -0,0 +1,13 @@
+names = ['Ahmed', 'Kamal', 'Mariam','Mohammed']
+
+msg = "Hello, " + names[0].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[1].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[2].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[3].title() + "!"
+print(msg)
