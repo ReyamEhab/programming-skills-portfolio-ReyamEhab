a71910ff6bb34bd770acf2813ef6de98479a31f2
ReyamEhab <reyam_kama26@yahoo.com>
2023-10-22 17:03:12 +0400

    Exercise 6

--- Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 6.py
+++ Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 6.py
@@ -1,81 +1,47 @@
-# Invite some people to dinner.
-guests = ['Kamal', 'Ahmed', 'Mariam']
+guests = ['Kamal', 'Ahmed', 'Mariam', 'fatima']
+name = guests[0]
+print(name+ ", please come to dinner.")
 
-name = guests[0].title()
+name = guests[1]
 print(name + ", please come to dinner.")
 
-name = guests[1].title()
-print(name + ", please come to dinner.")
+name = guests[2]
+print(name+ ", please come to dinner.")
 
-name = guests[2].title()
-print(name + ", please come to dinner.")
+name = guests[3]
+print(name+ ", please come to dinner.")
 
-name = guests[1].title()
+name = guests[1]
 print("\nSorry, " + name + " can't make it to dinner.")
 
-# Jack can't make it! Let's invite Gary instead.
 del(guests[1])
-guests.insert(1, 'gary snyder')
+guests.insert(1, 'Maha')
 
-# Print the invitations again.
-name = guests[0].title()
+name = guests[0]
 print("\n" + name + ", please come to dinner.")
 
-name = guests[1].title()
+name = guests[1]
 print(name + ", please come to dinner.")
 
-name = guests[2].title()
+name = guests[2]
 print(name + ", please come to dinner.")
 
-# We got a bigger table, so let's add some more people to the list.
-print("\nWe got a bigger table!")
-guests.insert(0, 'frida kahlo')
-guests.insert(2, 'reinhold messner')
-guests.append('elizabeth peratrovich')
-
-name = guests[0].title()
+name = guests[3]
 print(name + ", please come to dinner.")
 
-name = guests[1].title()
-print(name + ", please come to dinner.")
-
-name = guests[2].title()
-print(name + ", please come to dinner.")
-
-name = guests[3].title()
-print(name + ", please come to dinner.")
-
-name = guests[4].title()
-print(name + ", please come to dinner.")
-
-name = guests[5].title()
-print(name + ", please come to dinner.")
-
-# Oh no, the table won't arrive on time!
 print("\nSorry, we can only invite two people to dinner.")
+name = guests.pop()
+print(f"Sorry, {name.title()} there's no room at the table.")
 
 name = guests.pop()
-print("Sorry, " + name.title() + " there's no room at the table.")
+print(f"Sorry, {name.title()} there's no room at the table.")
 
-name = guests.pop()
-print("Sorry, " + name.title() + " there's no room at the table.")
-
-name = guests.pop()
-print("Sorry, " + name.title() + " there's no room at the table.")
-
-name = guests.pop()
-print("Sorry, " + name.title() + " there's no room at the table.")
-
-# There should be two people left. Let's invite them.
 name = guests[0].title()
-print(name + ", please come to dinner.")
+print(f"{name}, please come to dinner.")
 
 name = guests[1].title()
-print(name + ", please come to dinner.")
+print(f"{name}, please come to dinner.")
 
-# Empty out the list.
-del(guests[0])
-del(guests[0])
+ 
+ 
 
-# Prove the list is empty.
-print(guests)
