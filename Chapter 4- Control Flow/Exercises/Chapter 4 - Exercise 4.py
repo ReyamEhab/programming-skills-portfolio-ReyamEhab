910029e5103dfcd49bd378685dec61b280fb8bfe
ReyamEhab <reyam_kama26@yahoo.com>
2023-10-24 11:11:54 +0400

    Chapter 4.py

---
+++ Chapter 4- Control Flow/Exercises/Chapter 4 - Exercise 4.py
@@ -0,0 +1,13 @@
+age = 40
+if age < 2:
+    print("you're a baby")
+elif age < 4:
+    print("you're a toddler") 
+elif age < 13:
+    print("you're a kid") 
+elif age < 20:
+    print("you're a teenager") 
+elif age < 65:
+    print("you're an adult") 
+else:
+    print("you're an elder") 
