999c8aa1243795cc5ae1e7993c12780e39446b4a
ReyamEhab <reyam_kama26@yahoo.com>
2023-11-04 13:35:40 +0400

    Chapter 5

---
+++ Chapter 5- Dictionaries/Exercises/Chapter 5 - Exercise 1.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Reyam',
+    'last_name': 'Ehab',
+    'age':20,
+    'city': 'RAK',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
