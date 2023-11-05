681e1bf1e99e181c3101e5f5d75f2a406c70d7b4
ReyamEhab <reyam_kama26@yahoo.com>
2023-11-05 08:11:18 +0400

    Chapter 5 .py

---
+++ Chapter 5- Dictionaries/Exercises/Chapter 5 - Exercise 2.py
@@ -0,0 +1,18 @@
+glossary = {
+    'string': 'a collection of alphabets, words or other characters.',
+    'comment': ' the inclusion of short descriptions along with the code to increase its readability.',
+    'list': ' a data structure in Python that is a mutable, or changeable, ordered sequence of elements.',
+    'if statment': ' a condition statement used to check a condition, and execute it if the condition holds true.',
+    'Loop': "repeating something over and over until a particular condition is satisfieds.",
+    }
+
+name = 'string'
+print("\n" + name + ": " + glossary[name])
+name = 'comment'
+print("\n" + name + ": " + glossary[name])
+name = 'list'
+print("\n" + name + ": " + glossary[name])
+name = 'if statment'
+print("\n" + name + ": " + glossary[name])
+name = 'Loop'
+print("\n" + name + ": " + glossary[name])
