8a1d3662f896f071337888987e0ae5a9f43647fa
ReyamEhab <reyam_kama26@yahoo.com>
2023-11-05 08:23:07 +0400

    Chapter 5.py

---
+++ Chapter 5- Dictionaries/Exercises/Chapter 5 - Exercise 3.py
@@ -0,0 +1,14 @@
+glossary = {
+    'string': 'a collection of alphabets, words or other characters.',
+    'comment': ' the inclusion of short descriptions along with the code to increase its readability.',
+    'list': ' a data structure in Python that is a mutable, or changeable, ordered sequence of elements.',
+    'if statment': ' a condition statement used to check a condition, and execute it if the condition holds true.',
+    'Loop': "repeating something over and over until a particular condition is satisfieds.",
+    'float':"a function or reusable code in the Python programming language that converts values into floating point numbers.",
+    'integer':" whole number, positive or negative, without decimals, of unlimited length.",
+    'variable':" is a reserved memory location to store values.",
+    'tuple':" a type of data structure that is very similar to lists. The main difference between the two is that tuples are immutable.",
+    'dictionary':" is a collection of key-value pairs.",
+    }
+for name, definition in glossary.items():
+    print("\n" + name + ": " + definition)
