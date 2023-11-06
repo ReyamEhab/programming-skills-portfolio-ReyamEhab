ed279a221f3a72a4afc5e461362842d22343b5c8
ReyamEhab <reyam_kama26@yahoo.com>
2023-11-06 09:44:03 +0400

    Chapter 2 - practice.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter 2 - practice 3.py
@@ -0,0 +1,9 @@
+input_value = input("Enter a value: ")
+
+string_value = str(input_value)
+integer_value = int(input_value)
+float_value = float(input_value)
+ 
+print(f"The string value is: {string_value}")
+print(f"The integer value is: {integer_value}")
+print(f"The float value is: {float_value}")
