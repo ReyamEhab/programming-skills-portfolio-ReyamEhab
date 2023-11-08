374392b9e4bea861ea274fbea32bb82bc7c698ff
ReyamEhab <144321212+ReyamEhab@users.noreply.github.com>
2023-11-08 20:59:39 +0400

    Update Chapter 5 - practice 3.py

--- Chapter 5- Dictionaries/Chapter 5 - practice 3.py
+++ Chapter 5- Dictionaries/Chapter 5 - practice 3.py
@@ -1,3 +1,4 @@
-my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4} 
+my_dict = {'key1': 5, 'key2': 8, 'key3': 1, 'key4': 2}
+# Iterating through the values of the dictionary
 for value in my_dict.values():
     print(value)
